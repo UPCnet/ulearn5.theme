fc9aaa887460b34e3d517b9ba1b1e573d01256f3
alberto.duran <alberto.duran@upcnet.es>
2017-11-22 12:23:17 +0100

    Unify homebuttonbar & custombuttonbar to buttonbar in tiles

---
+++ src/ulearn5/theme/portlets/buttonbar.py
@@ -0,0 +1,62 @@
+from zope.interface import implements
+from plone import api
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFPlone import PloneMessageFactory as _
+import transaction
+
+
+class IButtonBarPortlet(IPortletDataProvider):
+    """ A portlet which can render the logged user profile information.
+    """
+
+
+class Assignment(base.Assignment):
+    implements(IButtonBarPortlet)
+
+    title = _(u'buttonbar', default=u'Button bar')
+
+
+class Renderer(base.Renderer):
+
+    render = ViewPageTemplateFile('templates/buttonbar.pt')
+
+    def __init__(self, context, request, view, manager, data):
+        super(Renderer, self).__init__(context, request, view, manager, data)
+        self.portal_url = api.portal.get().absolute_url()
+
+    def is_activate_sharedwithme(self):
+        if (api.portal.get_registry_record('base5.core.controlpanel.core.IGenwebCoreControlPanelSettings.elasticsearch') != None) and (api.portal.get_registry_record('ulearn5.core.controlpanel.IUlearnControlPanelSettings.activate_sharedwithme') == True):
+            portal = api.portal.get()
+            if portal.portal_actions.object.local_roles.visible == False:
+                portal.portal_actions.object.local_roles.visible = True
+                transaction.commit()
+            return True
+        else:
+            return False
+
+    def is_activate_news(self):
+        return api.portal.get_registry_record('ulearn5.core.controlpanel.IUlearnControlPanelSettings.activate_news')
+
+    def getClass(self):
+        """ Returns class for links """
+        shared = self.is_activate_sharedwithme()
+        news = self.is_activate_news()
+        width = ''
+        if shared and news:
+            width = 'col-md-3'
+        elif shared and not news:
+            width = 'col-md-4'
+        elif news and not shared:
+            width = 'col-md-4'
+        else:
+            width = 'col-md-6'
+
+        return "active bubble top " + width
+
+
+class AddForm(base.NullAddForm):
+
+    def create(self):
+        return Assignment()
