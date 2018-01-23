14102ab695839c0c74c63d84738b2cb8b027ba54
alberto.duran <alberto.duran@upcnet.es>
2018-01-23 10:29:03 +0100

    Tiles eliminadas

--- src/ulearn5/theme/browser/tiles/flashesinformativos/flashesinformativos.js
+++
@@ -1,8 +0,0 @@
-  $(document).ready(function (event) {
-      intervalflash = setInterval(function (event) {
-          $('.ulearnboxflash .carousel').carousel('cycle')
-          clearInterval(intervalflash)
-          console.log('intervalflash')
-      }, 2000)
-
-  })
