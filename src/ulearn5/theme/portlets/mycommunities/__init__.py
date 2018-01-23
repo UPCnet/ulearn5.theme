f01cc0efecbe850107d0830d5cd6177c59f91bd2
alberto.duran <alberto.duran@upcnet.es>
2018-01-23 10:25:42 +0100

    Portlet de mycommunities borrado

--- src/ulearn5/theme/portlets/mycommunities/__init__.py
+++
@@ -1,5 +0,0 @@
-from mycommunities import IMyCommunitiesPortlet
-from mycommunities import Assignment
-from mycommunities import Renderer
-from mycommunities import AddForm
-from mycommunities import EditForm
