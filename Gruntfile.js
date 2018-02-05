e42808074f9e04fe8ba571eab3614c33953b4b1e
alberto.duran <alberto.duran@upcnet.es>
2018-02-05 13:37:34 +0100

    update gruntfile for include angular css components

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,6 @@
 module.exports = function (grunt) {
     'use strict';
 
-    var uglify_options = {
-        sourceMap: true };
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         // we could just concatenate everything, really
@@ -37,11 +34,11 @@ module.exports = function (grunt) {
             ulearn: {
                 options: {
                     sassDir: 'stylesheets/ulearn',
-                    cssDir: 'css',
+                    cssDir: 'css/ulearn/',
                     outputStyle: 'compressed'
                 },
                 files: {
-                    'css/ulearn.css': 'stylesheets/ulearn/ulearn.scss'
+                    'css/ulearn/ulearn.css': 'stylesheets/ulearn/ulearn.scss'
                 }
             }
         },
@@ -53,6 +50,28 @@ module.exports = function (grunt) {
                 src: ['css/bootstrap.css', 'css/upcnet.css'],
                 dest: 'css/homeupc-compiled.css',
             },
+            ulearn: {
+                src: ['../../../../../../ulearn5.js/ulearn5/js/components/angular-ui-select/dist/select.css',
+                "../../../../../../ulearn5.js/ulearn5/js/components/selectize/dist/css/selectize.default.css",
+                "../../../../../../ulearn5.js/ulearn5/js/components/selectize/dist/css/selectize.bootstrap2.css",
+                '../../../../../../ulearn5.js/ulearn5/js/components/angular-datatables/dist/plugins/bootstrap/datatables.bootstrap.css',
+                '../../../../../../ulearn5.js/ulearn5/js/components/ngDialog/css/ngDialog.css',
+                '../../../../../../ulearn5.js/ulearn5/js/components/ngDialog/css/ngDialog-theme-default.css',
+                '../../../../../../ulearn5.js/ulearn5/js/components/ngDialog/css/ngDialog-theme-plain.css',
+                '../../../../../../ulearn5.js/ulearn5/js/components/sweetalert/lib/sweet-alert.css',
+                '../../../../../../ulearn5.js/ulearn5/js/components/v-modal/dist/v-modal.css',
+                '../../portlets/importantnews/importantnews.css',
+                '../../portlets/flashesinformativos/flashesinformativos.css',
+                '../../portlets/mysubjects/mysubjects.css',
+                'css/ulearn/ulearn.css'],
+                dest: 'css/ulearn/ulearn-compiled.css',
+            },
+        },
+        cssmin: {
+            target : {
+				src : ["css/ulearn/ulearn-compiled.css"],
+				dest : "css/ulearn-compiled.min.css"
+			}
         },
         watch: {
             upc: {
@@ -69,17 +88,16 @@ module.exports = function (grunt) {
                     'stylesheets/bootstrap.scss',
                     'stylesheets/upcnet.scss',
                 ],
-                tasks: ['compass:homeupc', 'concat']
+                tasks: ['compass:homeupc', 'concat:dist']
             },
             ulearn: {
                 files: [
                     'stylesheets/ulearn/*.scss',
                     'stylesheets/ulearn.scss'
                 ],
-                tasks: ['compass:ulearn']
+                tasks: ['compass:ulearn', 'concat:ulearn', 'cssmin']
             }
         },
-
         browserSync: {
             plone: {
                 bsFiles: {
@@ -102,9 +120,9 @@ module.exports = function (grunt) {
     // grunt.loadTasks('tasks');
     grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // CWD to theme folder
     grunt.file.setBase('./src/ulearn5/theme/theme/assets');
@@ -112,5 +130,4 @@ module.exports = function (grunt) {
     grunt.registerTask('default', ["browserSync:plone", "watch"]);
     grunt.registerTask('bsync', ["browserSync:html", "watch"]);
     grunt.registerTask('plone-bsync', ["browserSync:plone", "watch"]);
-    grunt.registerTask('minify', ['uglify']);
 };
