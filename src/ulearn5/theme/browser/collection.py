c0fa89afe8a3f593bdb64cd9383c0c5c20b691f9
alberto.duran <alberto.duran@upcnet.es>
2018-03-22 09:05:15 +0100

    Override listing_summary for folderviews and colectionviews

---
+++ src/ulearn5/theme/browser/collection.py
@@ -0,0 +1,31 @@
+from plone.app.contenttypes.browser.collection import CollectionView
+
+
+class MyCollectionView(CollectionView):
+
+    def abrevia(self, summary, sumlenght):
+        """ Retalla contingut de cadenes """
+
+        bb = ''
+
+        if sumlenght < len(summary):
+            bb = summary[:sumlenght]
+
+            lastspace = bb.rfind(' ')
+            cutter = lastspace
+            precut = bb[0:cutter]
+
+            if precut.count('<b>') > precut.count('</b>'):
+                cutter = summary.find('</b>', lastspace) + 4
+            elif precut.count('<strong>') > precut.count('</strong>'):
+                cutter = summary.find('</strong>', lastspace) + 9
+            bb = summary[0:cutter]
+
+            if bb.count('<p') > precut.count('</p'):
+                bb += '...</p>'
+            else:
+                bb = bb + '...'
+        else:
+            bb = summary
+
+        return bb
