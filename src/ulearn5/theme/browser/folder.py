17f0373f94ed0e37c0f0cb648f9e08822059fb0c
root@comunitatsdevel <plone.team@upcnet.es>
2018-03-27 17:05:34 +0200

    Maquetacio vistes

--- src/ulearn5/theme/browser/folder.py
+++ src/ulearn5/theme/browser/folder.py
@@ -3,29 +3,33 @@ from plone.app.contenttypes.browser.folder import FolderView
 
 class MyFolderView(FolderView):
 
-    def abrevia(self, summary, sumlenght):
+    def abrevia(self, obj, sumlenght):
         """ Retalla contingut de cadenes """
-        
-        bb = ''
+        text = getattr(obj, 'text', None)
+        if text is not None:
+            summary = text.output
+            bb = ''
 
-        if sumlenght < len(summary):
-            bb = summary[:sumlenght]
+            if sumlenght < len(summary):
+                bb = summary[:sumlenght]
 
-            lastspace = bb.rfind(' ')
-            cutter = lastspace
-            precut = bb[0:cutter]
+                lastspace = bb.rfind(' ')
+                cutter = lastspace
+                precut = bb[0:cutter]
 
-            if precut.count('<b>') > precut.count('</b>'):
-                cutter = summary.find('</b>', lastspace) + 4
-            elif precut.count('<strong>') > precut.count('</strong>'):
-                cutter = summary.find('</strong>', lastspace) + 9
-            bb = summary[0:cutter]
+                if precut.count('<b>') > precut.count('</b>'):
+                    cutter = summary.find('</b>', lastspace) + 4
+                elif precut.count('<strong>') > precut.count('</strong>'):
+                    cutter = summary.find('</strong>', lastspace) + 9
+                bb = summary[0:cutter]
 
-            if bb.count('<p') > precut.count('</p'):
-                bb += '...</p>'
+                if bb.count('<p') > precut.count('</p'):
+                    bb += '...</p>'
+                else:
+                    bb = bb + '...'
             else:
-                bb = bb + '...'
-        else:
-            bb = summary
+                bb = summary
 
-        return bb
+            return bb
+        else:
+            return ""
