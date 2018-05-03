77e3245e7c292c41385318648f5db0e6938ce7c5
alberto.duran <alberto.duran@upcnet.es>
2018-05-03 12:35:59 +0200

    login owncloud fail

--- src/ulearn5/theme/theme/assets/javascripts/afterall.js
+++ src/ulearn5/theme/theme/assets/javascripts/afterall.js
@@ -1,8 +1,44 @@
-
 $(document).ready(function () {
-    debugger;
-    if (window.screen.width < '768') {
-        $('body').removeClass('plone-toolbar-expanded');
-        $('body').addClass('plone-toolbar');
-    }
-})
+  $("#sso").click(function (data) {
+    // var u = $("#__ac_name").val();
+    // var p = $("#__ac_password").val();
+    // var formData = new FormData();
+    // formData.append("username", u);
+    // formData.append("password", p);
+    // debugger;
+    // $("#u_own").val(u);
+    // $("#p_own").val(p);
+    // $("#login_own").submit();
+    // $.post("http://pc60012.estacions.upcnet.es/index.php/apps/loginviapost/login",
+    //   {username: u, password: p})
+    //   .done(function(aux) {
+    //     alert(aux);
+    //     alert( "second success" );
+    //   })
+    //   .fail(function() {
+    //     alert( "error" );
+    //   })
+  });
+});
+// (function() {
+//   function myFunction() {
+//     var u = document.getElementById("__ac_name").value
+//     var p = document.getElementById("__ac_password").value
+//     var formData = new FormData();
+//     formData.append("username", u);
+//     formData.append("password", p);
+//     debugger;
+//     var xmlHttp = new XMLHttpRequest();
+//         xmlHttp.onreadystatechange = function() {
+//           if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
+//             alert(xmlHttp.responseText);
+//         }
+//         xmlHttp.open("post", "http://pc60012.estacions.upcnet.es/index.php/apps/loginviapost/login");
+//         xmlHttp.send(formData);
+//     // $.ajax({
+//     //   url: "http://pc60012.estacions.upcnet.es/index.php/apps/loginviapost/login",
+//     //   method: "POST",
+//     //   data: {username: "victor", password: "123123"}
+//     // });
+//   };
+// })();
