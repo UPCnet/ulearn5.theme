25af1eb17d547891bb1120ae4ae4f42a46abbbe5
iago.lopez <iago.lopez@upcnet.es>
2018-06-01 08:54:53 +0200

    Añadir botón para ir a la parte superior de la página

--- src/ulearn5/theme/theme/assets/javascripts/theming5.js
+++ src/ulearn5/theme/theme/assets/javascripts/theming5.js
@@ -231,4 +231,21 @@ $(document).ready(function () {
       }, 15000);
     }
 
+    // Button scroll up of the page
+    var offset = 250;
+    var duration = 300;
+    $(window).scroll(function() {
+        if($(this).scrollTop() > offset){
+            $('.back-to-top').fadeIn(duration);
+        }else{
+            $('.back-to-top').fadeOut(duration);
+        }
+    });
+
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+
 }); //ready
