4da7098bc48ea262b0059eaf573aec19b06711c8
iago.lopez <iago.lopez@upcnet.es>
2018-07-09 12:43:02 +0200

    Flashes informativos (carrousel + fade + marquee)

--- src/ulearn5/theme/portlets/flashesinformativos/flashesinformativos.js
+++ src/ulearn5/theme/portlets/flashesinformativos/flashesinformativos.js
@@ -1,8 +1,13 @@
-  $(document).ready(function (event) {
-      intervalflash = setInterval(function (event) {
-          $('.ulearnboxflash .carousel').carousel('cycle')
-          clearInterval(intervalflash)
-          console.log('intervalflash')
-      }, 2000)
+$(document).ready(function (event) {
 
-  })
+    $('.ulearnboxflash .carousel').carousel({
+        interval: $('#flasinformatiu').attr('data-duration')
+    });
+
+/*    $('#flasinformatiu .marquee').marquee({
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true
+    });*/
+
+});
