fecb4781a0d8c26c82208a24c49bd4507cf91728
iago.lopez <iago.lopez@upcnet.es>
2018-07-16 11:33:19 +0200

    Portlet Smart

--- src/ulearn5/theme/portlets/smart/javascripts/smartYT.js
+++ src/ulearn5/theme/portlets/smart/javascripts/smartYT.js
@@ -1,47 +1,45 @@
-$( document ).ready(function(){
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function loadYTVideos(){
+  $('.player').each(function( index ) {
+    var idYT = $(this).attr("data-YT");
+    function onYouTubeIframeAPIReady() {
+      player[idYT] = new YT.Player('player-'+idYT, {
+        videoId: idYT,
+      });
+    }
+    try {
+      onYouTubeIframeAPIReady();
+    }catch(err){
+       console.log('%c' + err, 'color: #ff0000');
+      $("#player-"+idYT).html("\
+        <div class='errorYT'>\
+          <p>Error loading Youtube</p>\
+          <button onclick='loadYTVideos()'>Click to reload</button>\
+        </div>\
+      ");
+    }
+  });
+}
 
-  var player = new Array();
-
-  setTimeout(function(){
-    loadYTVideos();
-  }, 1500);
-
-  function loadYTVideos(){
-    $('.player').each(function( index ) {
-      var idYT = $(this).attr("data-YT");
-      function onYouTubeIframeAPIReady() {
-        player[idYT] = new YT.Player('player-'+idYT, {
-          videoId: idYT,
-        });
-      }
-      try {
-        onYouTubeIframeAPIReady();
-      }catch(err){
-         console.log('%c' + err, 'color: #ff0000');
-        $("#player-"+idYT).html("\
-          <div class='errorYT'>\
-            <p>Error loading Youtube</p>\
-            <button onclick='loadYTVideos()'>Click to reload</button>\
-          </div>\
-        ");
-      }
-    });
+function pauseYT(){
+  var youtubeActive = $('.carousel-video-yt.active iframe');
+  var videoActive = $('.carousel-video.active video')[0];
+  if(youtubeActive[0]){
+    var idYT = youtubeActive.attr("data-yt");
+    player[idYT].pauseVideo();
   }
 
-  function pause(){
-    var youtubeActive = $('.carousel-video-yt.active iframe');
-    var videoActive = $('.carousel-video.active video')[0];
-    if(youtubeActive[0]){
-      var idYT = youtubeActive.attr("data-yt");
-      player[idYT].pauseVideo();
-    }
-
-    if(videoActive){
-      videoActive.pause();
-    }
+  if(videoActive){
+    videoActive.pause();
   }
-});
+}
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player = new Array();
+
+setTimeout(function(){
+  loadYTVideos();
+}, 1500);
