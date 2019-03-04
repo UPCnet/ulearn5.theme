d25dc77cfd3a4c271cc30a395d0ccfee91369b97
Pilar Marinas <pilar.marinas@upcnet.es>
2019-03-04 11:18:15 +0100

    Back to development: 0.34

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='ulearn5.theme',
-    version='0.33',
+    version='0.34.dev0',
     description="Tema Comunitats Plone 5",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
