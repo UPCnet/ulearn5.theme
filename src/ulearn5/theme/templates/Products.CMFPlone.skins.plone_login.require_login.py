3aa8c478b5f55f14f295af7b10eaf2028f81b6f3
Pilar Marinas <pilar.marinas@upcnet.es>
2021-07-14 17:15:07 +0200

    Redirect login microsoft azure in require_login

--- src/ulearn5/theme/templates/Products.CMFPlone.skins.plone_login.require_login.py
+++ src/ulearn5/theme/templates/Products.CMFPlone.skins.plone_login.require_login.py
@@ -14,9 +14,28 @@ portal = context.portal_url.getPortalObject()
 # this would be the way to get the value of came_from
 #url = portal.getCurrentUrl()
 #context.REQUEST.set('came_from', url)
+from DateTime import DateTime
+
+# Descomentar para poder debuggar. Como no se puede poner ipdb con loggers
+# import logging
+# logger = logging.getLogger('azure')
+# try:
+#    import urllib
+# except Exception as e:
+#    logger.error('YYY ERROR IMPORT REQUEST {}'.format(str(e)))
+#    pass
 
 if context.portal_membership.isAnonymousUser():
-    return portal.restrictedTraverse(login)()
+    if 'acl_msal' in portal.acl_users:
+       acl_msal = portal.acl_users['acl_msal']
+       portal_url = context.portal_url()
+       portal_url_came_from = context.REQUEST.get('came_from', portal_url)
+       zope_DT = DateTime()
+       uuid = ''.join([a for a in str(zope_DT) if a.isalnum()])
+       url_azure = acl_msal.AUTHORITY + '/oauth2/v2.0/authorize?scope=' + str(''.join(acl_msal.SCOPE)) + '+offline_access+openid+profile&state=' + str(uuid) + '|' + portal_url_came_from + '&redirect_uri=' + portal_url + acl_msal.REDIRECT_PATH + '&response_type=code&client_id=' + acl_msal.CLIENT_ID
+       context.REQUEST.response.redirect(url_azure)
+    else:
+       return portal.restrictedTraverse(login)()
 else:
     from AccessControl import getSecurityManager
     if getSecurityManager().checkPermission("zope2.View", context):
