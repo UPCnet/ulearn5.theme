a24c027d1ca481e28fb44c03a681bdcc30056efc
Alberto Durán <alberto.duran@ithinkupc.com>
2025-01-20 12:27:05 +0100

    [UPD] profile portlets code formatted

--- src/ulearn5/theme/portlets/profile/profile.py
+++ src/ulearn5/theme/portlets/profile/profile.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_chain
 from Acquisition import aq_inner
-from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -12,7 +11,6 @@ from plone.app.portlets.portlets import base
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.registry.interfaces import IRegistry
 from zope.component import getMultiAdapter
-from zope.component import getUtility
 from zope.component import queryUtility
 from zope.interface import implementer
 
@@ -29,18 +27,18 @@ from souper.soup import get_soup
 
 
 class IProfilePortlet(IPortletDataProvider):
-    """ A portlet which can render the logged user profile information. """
+    """A portlet which can render the logged user profile information."""
 
 
 @implementer(IProfilePortlet)
 class Assignment(base.Assignment):
 
-    title = _('profile', default='User profile')
+    title = _("profile", default="User profile")
 
 
 class Renderer(base.Renderer):
 
-    render = ViewPageTemplateFile('profile.pt')
+    render = ViewPageTemplateFile("profile.pt")
 
     def __init__(self, context, request, view, manager, data):
         super(Renderer, self).__init__(context, request, view, manager, data)
@@ -54,14 +52,14 @@ class Renderer(base.Renderer):
         return True
 
     def is_admin_user(self):
-        if api.user.get_current().id == 'admin':
+        if api.user.get_current().id == "admin":
             return True
         else:
             return False
 
     def fullname(self):
         if self.user_info:
-            fullname = self.user_info.get('fullname', '')
+            fullname = self.user_info.get("fullname", "")
         else:
             fullname = None
 
@@ -71,18 +69,18 @@ class Renderer(base.Renderer):
             return self.username
 
     def get_portrait(self):
-        pm = api.portal.get_tool('portal_membership')
+        pm = api.portal.get_tool("portal_membership")
         return pm.getPersonalPortrait().absolute_url()
 
     def has_complete_profile(self):
         if self.user_info:
-            id = self.user_info['id']
+            id = self.user_info["id"]
             portal = api.portal.get()
-            soup_users_portrait = get_soup('users_portrait', portal)
-            exist = [r for r in soup_users_portrait.query(Eq('id_username', id))]
+            soup_users_portrait = get_soup("users_portrait", portal)
+            exist = [r for r in soup_users_portrait.query(Eq("id_username", id))]
             if exist:
                 user_record = exist[0]
-                return user_record.attrs['portrait']
+                return user_record.attrs["portrait"]
             else:
                 return False
         else:
@@ -91,10 +89,10 @@ class Renderer(base.Renderer):
             return False
 
     def has_webmaster_role(self):
-        return 'WebMaster' in api.user.get_roles()
+        return "WebMaster" in api.user.get_roles()
 
     def get_badges(self):
-        """ Done consistent with an hipotetical badge provider backend """
+        """Done consistent with an hipotetical badge provider backend"""
         # Call to the REST service for the user badges returning a list with the
         # (for example 4 more recent badges or the user selected badges)
         # >>> connect_backpack(self.username.getId(), app="ulearn", sort="user_preference", limit=4)
@@ -102,19 +100,19 @@ class Renderer(base.Renderer):
 
         badges = deepcopy(AVAILABLE_BADGES)
         if self.has_complete_profile():
-            badges[0]['awarded'] = True
+            badges[0]["awarded"] = True
 
         registry = queryUtility(IRegistry)
         settings = registry.forInterface(IUlearnControlPanelSettings, check=False)
 
         thinnkins = self.get_thinnkins()
         if thinnkins:
-            if (thinnkins >= int(settings.threshold_winwin1)):
-                badges[1]['awarded'] = True
-            if (thinnkins >= int(settings.threshold_winwin2)):
-                badges[2]['awarded'] = True
-            if (thinnkins >= int(settings.threshold_winwin3)):
-                badges[3]['awarded'] = True
+            if thinnkins >= int(settings.threshold_winwin1):
+                badges[1]["awarded"] = True
+            if thinnkins >= int(settings.threshold_winwin2):
+                badges[2]["awarded"] = True
+            if thinnkins >= int(settings.threshold_winwin3):
+                badges[3]["awarded"] = True
 
         return badges
 
@@ -123,22 +121,40 @@ class Renderer(base.Renderer):
         settings = registry.forInterface(IUlearnControlPanelSettings, check=False)
 
         info = {
-            'profile': {
-                'title': _('Complete the profile'),
-                'num': 0,
+            "profile": {
+                "title": _("Complete the profile"),
+                "num": 0,
             },
-            'winwin1': {
-                'title': self.context.translate(_('badge_title', default='Contribute ${num} posts', mapping={'num': settings.threshold_winwin1})),
-                'num': settings.threshold_winwin1,
+            "winwin1": {
+                "title": self.context.translate(
+                    _(
+                        "badge_title",
+                        default="Contribute ${num} posts",
+                        mapping={"num": settings.threshold_winwin1},
+                    )
+                ),
+                "num": settings.threshold_winwin1,
             },
-            'winwin2': {
-                'title': self.context.translate(_('badge_title', default='Contribute ${num} posts', mapping={'num': settings.threshold_winwin2})),
-                'num': settings.threshold_winwin2,
+            "winwin2": {
+                "title": self.context.translate(
+                    _(
+                        "badge_title",
+                        default="Contribute ${num} posts",
+                        mapping={"num": settings.threshold_winwin2},
+                    )
+                ),
+                "num": settings.threshold_winwin2,
             },
-            'winwin3': {
-                'title': self.context.translate(_('badge_title', default='Contribute ${num} posts', mapping={'num': settings.threshold_winwin3})),
-                'num': settings.threshold_winwin3,
-            }
+            "winwin3": {
+                "title": self.context.translate(
+                    _(
+                        "badge_title",
+                        default="Contribute ${num} posts",
+                        mapping={"num": settings.threshold_winwin3},
+                    )
+                ),
+                "num": settings.threshold_winwin3,
+            },
         }
 
         return info
@@ -159,30 +175,36 @@ class Renderer(base.Renderer):
 
     def userCheckDisplay(self):
         user = api.user.get_current()
-        return user.getProperty('visible_userprofile_portlet', True)
+        return user.getProperty("visible_userprofile_portlet", True)
 
     def showEditCommunity(self):
-        if not IPloneSiteRoot.providedBy(self.context) and \
-           ICommunity.providedBy(self.context) and \
-           'Owner' in api.user.get_roles(username=self.username, obj=self.context):
+        if (
+            not IPloneSiteRoot.providedBy(self.context)
+            and ICommunity.providedBy(self.context)
+            and "Owner" in api.user.get_roles(username=self.username, obj=self.context)
+        ):
             return True
 
     def get_addable_types(self):
-        factories_view = getMultiAdapter((self.context, self.request), name='folder_factories')
+        factories_view = getMultiAdapter(
+            (self.context, self.request), name="folder_factories"
+        )
         return factories_view.addable_types()
 
     def get_posts_literal(self):
-        literal = api.portal.get_registry_record(name='ulearn5.core.controlpanel.IUlearnControlPanelSettings.people_literal')
-        if literal == 'thinnkers':
-            return 'thinnkins'
+        literal = api.portal.get_registry_record(
+            name="ulearn5.core.controlpanel.IUlearnControlPanelSettings.people_literal"
+        )
+        if literal == "thinnkers":
+            return "thinnkins"
         else:
-            return 'entrades'
+            return "entrades"
 
     def get_hash(self, community):
         return sha1(community.absolute_url()).hexdigest()
 
     def get_url(self, community):
-        return self.portal_url + '/' + community.getPhysicalPath()[-1]
+        return self.portal_url + "/" + community.getPhysicalPath()[-1]
 
     def isCurrentPage(self, page):
         param = False
@@ -195,31 +217,31 @@ class Renderer(base.Renderer):
         return community.community_type
 
     def get_edit_literal(self):
-        """ Get string literal for edit """
+        """Get string literal for edit"""
         lang = pref_lang()
-        if 'ca' in lang:
+        if "ca" in lang:
             return "Editar la comunitat"
-        elif 'es' in lang:
+        elif "es" in lang:
             return "Editar la comunidad"
         else:
             return "Edit community"
 
     def get_acl_literal(self):
-        """ Get string literal for editacl """
+        """Get string literal for editacl"""
         lang = pref_lang()
-        if 'ca' in lang:
+        if "ca" in lang:
             return "Gestiona els membres"
-        elif 'es' in lang:
+        elif "es" in lang:
             return "Gestiona los miembros"
         else:
             return "Manage members"
 
     def get_com_type_literal(self):
-        """ Get string literal for community type """
+        """Get string literal for community type"""
         lang = pref_lang()
-        if 'ca' in lang:
+        if "ca" in lang:
             return "Canvia el tipus de comunitat"
-        elif 'es' in lang:
+        elif "es" in lang:
             return "Cambia el tipo de comunidad"
         else:
             return "Change community type"
@@ -227,6 +249,7 @@ class Renderer(base.Renderer):
     @memoize_contextless
     def portal_url(self):
         return self.portal().absolute_url()
+
 
 class AddForm(base.NullAddForm):
 
